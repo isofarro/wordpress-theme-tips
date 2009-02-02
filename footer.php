a016bd403ce7c0e38ada11ce5c44c7d69ab04fe7
Mike Davies <isofarro2@gmail.com>
2009-02-02 17:01:27 +0000

    Fix IE6 height bug

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 	<div id="y-foot">
 		<span>Copyright &copy; 2007 - 2009, <a href="http://www.isolani.co.uk">isolani</a></span>
 		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
-		<p><?php wp_footer(); ?></p>
+		<?php wp_footer(); ?>
 	</div>
 </div>
 
--- index.php
+++ index.php
@@ -16,25 +16,25 @@
 	</div>
 	<?php elseif ( is_day() ): ?>
 	<div class="mod">
-		<h1 class="hd"><?php echo bloginfo('name'); ?>: <?php the_time('l, F jS, Y'); ?></h1>
+		<h1 class="hd"><?php the_time('l, F jS, Y'); ?></h1>
 		<p class="bd">You are currently browsing the <a href="<?php bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> archives for the day <?php the_time('l, F jS, Y'); ?>.</p>	
 		<div class="ft"></div>	
 	</div>
 	<?php elseif ( is_month() ): ?>
 	<div class="mod">
-		<h1 class="hd"><?php echo bloginfo('name'); ?>: <?php the_time('F, Y'); ?></h1>
+		<h1 class="hd"><?php the_time('F, Y'); ?></h1>
 		<p class="bd">You are currently browsing the <a href="<?php bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> archives for <?php the_time('F, Y'); ?>.</p>
 		<div class="ft"></div>	
 	</div>
 	<?php elseif ( is_year() ): ?>
 	<div class="mod">
-		<h1 class="hd"><?php echo bloginfo('name'); ?>: <?php the_time('Y'); ?></h1>
+		<h1 class="hd"><?php the_time('Y'); ?></h1>
 		<p class="bd">You are currently browsing the <a href="<?php bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> archives for the year <?php the_time('Y'); ?>.</p>	
 		<div class="ft"></div>	
 	</div>
 	<?php elseif ( is_search() ): ?>
 	<div class="mod">
-		<h1 class="hd"><?php echo bloginfo('name'); ?>: '<?php the_search_query(); ?>'</h1>
+		<h1 class="hd"><?php echo bloginfo('name'); ?> Search: '<?php the_search_query(); ?>'</h1>
 		<p class="bd">You have searched the <a href="<?php echo bloginfo('url'); ?>/"><?php echo bloginfo('name'); ?></a> archives for <em>'<?php the_search_query(); ?>'</em>.</p>	
 		<div class="ft"></div>	
 	</div>
